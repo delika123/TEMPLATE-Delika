fb79e613a612ed320dd6dc3582370575d139b017
delika123 <delikasitumorang41@gmail.com>
2021-10-12 01:19:16 +0700

    .

---
+++ application/views/v_profile.php
@@ -0,0 +1,8 @@
+<section>
+    <h2>profile Page</h2>
+    <p>Selamat Datang Pada Halaman Profil</p> <br>
+    <p>Nama : Delika Situmorang</p> <br>
+    <p>NIM : 12201525</p> <br>
+    <p>Kelas : 12.3A.03</p> <br>
+    <p>Jurusan : Sistem Informasi</p>
+</section>
